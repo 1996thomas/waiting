d3fce0cf0c40349df7da80845cbe682af80093b6
thomas1996 <thomas.kanthack@gmail.com>
2025-03-19 09:30:05 +0100

    add meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen w-screen   font-[family-name:var(--font-geist-sans)] ">
+      <div className="flex flex-col items-center justify-center h-screen w-full   font-[family-name:var(--font-geist-sans)] ">
         <h1 className="text-[10vw] font-black">COOKIN UP 🍜...</h1>
-      </div>
         <Link href={"https://instagram.com/knit_paris"} className="flex justify-center">👀</Link>
+      </div>
     </>
   );
 }
