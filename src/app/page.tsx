6bd06b684c66d2835ce9d9727a9899499ebd4a25
thomas1996 <thomas.kanthack@gmail.com>
2025-05-23 10:26:51 +0200

    test beter ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,17 +40,17 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-end min-h-screen font-mono bg-[#141415] ">
+    <div className="flex flex-col items-center justify-end h-svh font-mono bg-[#141415] ">
       <video
         src="/anim.mp4"
         autoPlay
         loop
         playsInline
         muted
-        className="absolute inset-0 w-full h-full max-w-[600px] mx-auto object-cover brightness-55 "
+        className="absolute inset-0 w-full h-full z-0 max-w-[600px] mx-auto object-cover brightness-55 "
       />
-
-      <div className="mt-10 w-full h-[50vh] max-w-sm  gap-3 flex flex-col items-center justify-center z-10">
+<div className="flex-1"/>
+      <div className="mt-10 w-full flex-1 max-w-sm  gap-3 flex flex-col items-center justify-center z-10">
         {!submitted ? (
           <>
             <h1 className="font-bold uppercase">
@@ -61,7 +61,7 @@ export default function Home() {
                 e.preventDefault();
                 handleSubmit();
               }}
-              className="flex bg-white/15 rounded-full shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-sm border border-white/30 p-2 pl-5 items-center"
+              className="flex bg-white/15 rounded-full shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-sm border border-white/30 p-2 pl-5 z-10 items-center"
             >
               <input
                 type="email"
